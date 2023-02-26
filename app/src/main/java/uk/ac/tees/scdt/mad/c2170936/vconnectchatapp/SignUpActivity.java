f9781a12d43bac53fc83bce9b7cb62754d715e9b
DELL <ezevictor750@gmail.com>
2023-02-26 01:55:37 +0000

    Firebase Account Creation Successful

--- app/src/main/java/uk/ac/tees/scdt/mad/c2170936/vconnectchatapp/SignUpActivity.java
+++ app/src/main/java/uk/ac/tees/scdt/mad/c2170936/vconnectchatapp/SignUpActivity.java
@@ -7,16 +7,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUpActivity extends AppCompatActivity {
 
-    Button callLogin;
+    //Declaring variables
+    TextInputLayout regName, regEmail, regPhone, regPassword;
+    Button callLogin, regSignUpBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Hooks to all xml elements in activity_sign_up.xml
+        regName = findViewById(R.id.name);
+        regEmail = findViewById(R.id.email);
+        regPassword = findViewById(R.id.password);
+        regPhone = findViewById(R.id.phone);
+        regSignUpBtn = findViewById(R.id.regSignup);
         callLogin = findViewById(R.id.login);
+
+        //save data in Firebase on button click
+        regSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("User");
+
+                //Get all the vales
+                String name = regName.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+                String phone = regPhone.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name, email, phone, password);
+
+                reference.child(phone).setValue(helperClass);
+            }
+        });
 
         callLogin.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/uk/ac/tees/scdt/mad/c2170936/vconnectchatapp/UserHelperClass.java
@@ -0,0 +1,49 @@
+package uk.ac.tees.scdt.mad.c2170936.vconnectchatapp;
+
+public class UserHelperClass {
+
+    String name, email, phone, password;
+
+    public UserHelperClass() {
+    }
+
+    public UserHelperClass(String name, String email, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
